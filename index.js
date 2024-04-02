47c2a0c4aee28df946a90712e6a9f075e424f10e
SaiDrive <mail2saishankar@gmail.com>
2024-04-02 16:32:04 +0530

    code to showcase api fetch

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+window.addEventListener('load', () => {
+    const heading = document.createElement('h1');
+    heading.innerHTML = "I'm not listening yet !"
+
+    const root = document.querySelector('#root');
+
+    root.appendChild(heading);
+    heading.addEventListener('click', () =>{
+    
+        fetch('https://example-api-xvc9.onrender.com/getNewName')
+        .then(response => response.text())
+        .then(data => {
+            console.log('Text from API:', typeof data);
+            heading.innerHTML = `I'm ${data}`
+        })
+        .catch(error => {
+            console.error('Error fetching text:', error);
+        });
+
+    })
+
+})
