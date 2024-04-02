346d7e50c96931929240ed5c061d385ec237cdf7
SaiDrive <mail2saishankar@gmail.com>
2024-04-02 08:19:44 +0530

    an api to changes names created

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
 // Import required modules
 const express = require('express');
-
+const cors = require('cors');``
 // Create an instance of Express
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 // Routes
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
+
+const names = ['Ryan', 'Elon', 'Mark', 'Sam'];
+
+let i = 0;
+
+const updateName = (req, res) => {
+
+    if(i > names.length - 1){
+        i = 0
+    }
+
+    res.send(names[i++]);
+}
+
+app.get('/getNewName', updateName)
 
 
 
