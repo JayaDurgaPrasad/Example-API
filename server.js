a1a9f3b1faee7230d2f25083ecb4ba366f8c3335
SaiDrive <mail2saishankar@gmail.com>
2024-04-05 16:24:24 +0530

    testing cors mechanism

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 // Import required modules
 const express = require('express');
-const cors = require('cors');``
+const cors = require('cors');
 // Create an instance of Express
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+// app.use(cors());
 
 // Routes
 app.get('/', (req, res) => {
